a98df099fb44ae4fc69d3c14a93521f84f5ec1b8
marimorita <marimm0927@gmail.com>
2024-05-07 21:42:39 -0500

    header

---
+++ src/components/Layouts/Header/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const Header = () => {
+  return (
+    <div className='bg-black w-full h-full'>
+        <div className='text-white text-lg text-center font-semibold mt-2'>M O R I T A S T O R E
+         <div>                
+            <div className='flex text-white justify-center m-3'>mariana mendezmorales juan camilo corrales cedeño </div>
+         </div>
+        </div>
+    </div>
+  )
+}
